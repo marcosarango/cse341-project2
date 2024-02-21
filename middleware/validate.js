311eeecaf7d49d27c06c18917b66e594a94cedfe
marcosarango <sar21005@byui.edu>
2024-02-20 21:37:58 -0500

    authoritation and validation

--- middleware/validate.js
+++ middleware/validate.js
@@ -21,6 +21,28 @@ const saveUsers = (req, res, next) => {
   });
 };
 
+
+const saveLaptop = (req, res, next) => {
+  const validationRule = {
+    email: 'required|email',
+    username: 'required|string',
+    Name: 'required|string',
+    ipaddress: 'required|number',
+    
+  };
+  validator(req.body, validationRule, {}, (err, status) => {
+    if (!status) {
+      res.status(412).send({
+        success: false,
+        message: 'Validation failed',
+        data: err
+      });
+    } else {
+      next();
+    }
+  });
+};
 module.exports = {
-  saveUsers
+  saveUsers,
+  saveLaptop
 };
--- routes/laptop.js
+++ routes/laptop.js
@@ -1,7 +1,7 @@
 
 const express = require('express');
 const laptopsController = require('../controllers/laptop')
-//const validation = require('../middleware/validate')
+const validation = require('../middleware/validate')
 const authenticate = require('../middleware/authenticate')
 
 const router = express.Router()
@@ -9,10 +9,10 @@ const router = express.Router()
 router.get('/', laptopsController.getAll)
 router.get('/:id', laptopsController.getSingle)
 router.post(
-  '/', authenticate.isAuthenticated, laptopsController.createLaptop);
+  '/', authenticate.isAuthenticated, validation.saveLaptop, laptopsController.createLaptop);
   
 router.put(
-  '/:id', authenticate.isAuthenticated, laptopsController.updateLaptop);
+  '/:id', authenticate.isAuthenticated, validation.saveLaptop, laptopsController.updateLaptop);
 router.delete(
   '/:id',
   authenticate.isAuthenticated, laptopsController.deleteLaptop);
